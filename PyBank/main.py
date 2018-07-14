0d21954bfcc7732b3d0236d75ffa9c47a3407501
Kuldeep Jariwala <kuldeep@emolecules.com>
2018-07-13 21:21:44 -0700

    Python scripts for PyBank and PyPoll.

---
+++ PyBank/main.py
@@ -0,0 +1,101 @@
+# Import CSV library to read and write CSV
+import csv
+
+# Variable to hold value
+total_rows = 0
+total_amount = 0
+previous_amount = 0
+total_change_amount = 0
+greatest_increase_amount_value = 0
+greatest_increase_amount_date = ""
+greatest_decrease_amount_value = 0
+greatest_decrease_amount_date = ""
+
+# Open the budget_data.csv file using csv.DictReader
+with open('budget_data.csv', 'r') as csvfile:
+
+    budget_reader = csv.reader(csvfile, delimiter=',')
+
+    # Get the header from the file
+    header = next(budget_reader)
+
+    # Loop over all the rows in the CSV file
+    for row in budget_reader:
+
+        # increment the row count
+        total_rows += 1
+
+        # amount value from row[1]
+        amount = int(row[1])
+
+        # total amount is calculated using summation
+        total_amount += amount
+
+        # if row count is greater than 1, we calculate the change_amount
+        if total_rows > 1:
+
+            # change_amount is calculated using previous value
+            change_amount = amount - previous_amount
+
+            # Summation of the change amount
+            total_change_amount += change_amount
+
+            # Keep track of Greatest Increase in change
+            if change_amount > greatest_increase_amount_value:
+                greatest_increase_amount_value = change_amount
+                greatest_increase_amount_date = row[0]
+
+            # Keep track of Greatest Decrease in change
+            if change_amount < greatest_decrease_amount_value:
+                greatest_decrease_amount_value = change_amount
+                greatest_decrease_amount_date = row[0]
+
+        else:
+            change_amount = 0
+
+        # Assign the current amount to previous amount to calculate the change
+        previous_amount = amount
+
+# Variables to store print messages
+title = "Financial Analysis"
+separator = "-" * 30
+
+total_months_msg = 'Total Months: ' + str(total_rows)
+
+total_amount_msg = 'Total Amount: $ ' + str(total_amount)
+
+# Calculate the Average change between the each months for the whole period
+average_change = total_change_amount/(total_rows - 1)
+
+average_change_msg = 'Average Change: $ ' + str(round(average_change, 2))
+
+greatest_increase_amount_msg = 'Greatest Increase in Profits: '\
+                               + str(greatest_increase_amount_date)\
+                               + ' ($ ' + str(greatest_increase_amount_value) + ')'
+
+greatest_decrease_amount_msg = 'Greatest Decrease in Profits: '\
+                               + str(greatest_decrease_amount_date)\
+                               + ' ($ ' + str(greatest_decrease_amount_value) + ')'
+
+# Write the summary to the text file
+with open('budget_summary.txt', 'w') as textfile:
+
+    budget_summary_writer = csv.writer(textfile)
+
+    # Write all the rows
+    budget_summary_writer.writerow([title])
+    budget_summary_writer.writerow([separator])
+    budget_summary_writer.writerow([total_months_msg])
+    budget_summary_writer.writerow([total_amount_msg])
+    budget_summary_writer.writerow([average_change_msg])
+    budget_summary_writer.writerow([greatest_increase_amount_msg])
+    budget_summary_writer.writerow([greatest_decrease_amount_msg])
+
+# Print the summary to the terminal
+print(title)
+print(separator)
+print(total_months_msg)
+print(total_amount_msg)
+print(average_change_msg)
+print(greatest_increase_amount_msg)
+print(greatest_decrease_amount_msg)
---
+++ PyPoll/main.py
@@ -0,0 +1,82 @@
+# Import CSV library to read and write CSV
+import csv
+
+# Variable to hold value
+total_vote = 0
+voters_summary = dict()
+
+# Open the budget_data.csv file using csv.DictReader
+with open('election_data.csv', 'r') as csvfile:
+
+    election_reader = csv.reader(csvfile, delimiter=',')
+
+    # read the header
+    header = next(csvfile)
+
+    # Loop over all the rows in the CSV file
+    for row in election_reader:
+
+        # increment the row count
+        total_vote += 1
+
+        # Get the voter name from 2 row/col
+        voter_name = row[2]
+
+        # Check if this voter is in the dictionary
+        # then increment the value by 1
+        if voter_name in voters_summary:
+            voters_summary[voter_name] += 1
+        else:
+            # create new key in the dictionary and assign the value 1
+            voters_summary[voter_name] = 1
+
+# Variables to hold message
+title = 'Election Results'
+separator = '-' * 30
+total_vote_msg = 'Total Votes: ' + str(total_vote)
+final_winner = ''
+final_winner_total = 0
+each_voter_msg = ''
+
+# Loop over all the items in the dictionary
+for k, v in voters_summary.items():
+
+    # Calculate the percentage for each voter
+    vote_percent = (v / total_vote ) * 100
+
+    # Create a message for each voter
+    each_voter_msg += k + ': ' + str(round(vote_percent, 3)) + '% (' + str(v) + ')\n'
+
+    # Find the voter with the highest votes
+    if v > final_winner_total:
+        final_winner = k
+        final_winner_total = v
+
+# print the  messages
+final_winner_msg = 'Winner: ' + final_winner
+
+# Open a file to write the Election Summary
+election_summary_writer = open('election_summary.txt', 'w', newline="\n")
+
+# Write the summary to the text file
+election_summary_writer.write(title + '\n')
+election_summary_writer.write(separator + '\n')
+election_summary_writer.write(total_vote_msg + '\n')
+election_summary_writer.write(separator + '\n')
+election_summary_writer.write(each_voter_msg)
+election_summary_writer.write(separator + '\n')
+election_summary_writer.write(final_winner_msg + '\n')
+election_summary_writer.write(separator)
+
+# close the file writer
+election_summary_writer.close()
+
+# Print the summary to the terminal
+print(title)
+print(separator)
+print(total_vote_msg)
+print(separator)
+print(each_voter_msg)
+print(separator)
+print(final_winner_msg)
+print(separator)
